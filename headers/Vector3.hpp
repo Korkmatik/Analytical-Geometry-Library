7652eda861d7e3a2388f581cff6b709f7966abf3
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2018-05-13 00:39:41 +0200

    Small Fixes

--- headers/Vector3.hpp
+++ headers/Vector3.hpp
@@ -8,40 +8,38 @@
 
 	public:
 		//Constructors of the class
-		explicit Vector3(float x, float y, float z);					//3D-Vector
-		explicit Vector3(float xyz);									//Vector with three equal coordinates
-		explicit Vector3();												//makes a nullvector
-
-		~Vector3();														//Destructor of the class
-
-		//Setter
-		void setCoordinates(float x, float y, float z);					//sets new Coordinates for the Vector
+		explicit Vector3(float x, float y, float z);  //3D-Vector
+		explicit Vector3(float xyz);				  //Vector with three equal coordinates
+		explicit Vector3();							  //makes a nullvector
+														 
+		//Setter										 
+		void setCoordinates(float x, float y, float z);	 //sets new Coordinates for the Vector
+														 
+		//Getter										 
+		float getLenght() const;      //outputs the lenght of the Vector
+														 
+		float getXCoordinate() const; //outputs X-Coordinate
+		float getYCoordinate() const; //outputs Y-Coordinate
+		float getZCoordinate() const; //outputs Z-Coordinate
 		
-		//Getter	
-		float getLenght() const;										//outputs the lenght of the Vector
-
-		float getXCoordinate() const;									//outputs X-Coordinate
-		float getYCoordinate() const;									//outputs Y-Coordinate
-		float getZCoordinate() const;									//outputs Z-Coordinate
-		
-		float getAngle(Vector3 vector3) const;							//outputs the angle in which the two Vectors intersect [Degrees]
-		float getAngleWithXAxis() const;								//outputs the angle in which the Vector intersects the X-Axis [Degrees]
-		float getAngleWithYAxis() const;								//outputs the angle in which the Vector intersects the Y-Axis [Degrees]
-		float getAngleWithZAxis() const;								//outputs the angle in which the Vector intersects the Z-Axis [Degrees]
+		float getAngle(Vector3 vector3) const;	//outputs the angle in which the two Vectors intersect [Degrees]
+		float getAngleWithXAxis() const;		//outputs the angle in which the Vector intersects the X-Axis [Degrees]
+		float getAngleWithYAxis() const;		//outputs the angle in which the Vector intersects the Y-Axis [Degrees]
+		float getAngleWithZAxis() const;		//outputs the angle in which the Vector intersects the Z-Axis [Degrees]
 												 
-		bool isNullvector() const;										//return true if this Vecor is equal to the nullvector
-		bool isEqual(Vector3 vector3) const;		//returns true if the Vectors are lineary dependent
+		bool isNullvector() const;			 //return true if this Vecor is equal to the nullvector
+		bool isLinearlyDependent(Vector3 vector3) const; //returns true if the Vectors are lineary dependent
 
 		//Arithmetic operations
-		Vector3 operator+(Vector3& vector3);							//Adding two Vectors
-
-		Vector3 operator-(Vector3& vector3);							//Substracting two Vectors
-
-		Vector3 operator*(Vector3& vector3);							//Multiplication
+		Vector3 operator+(Vector3& vector3);  //Adding two Vectors
+											  
+		Vector3 operator-(Vector3& vector3);  //Substracting two Vectors
+											  
+		Vector3 operator*(Vector3& vector3);  //Multiplication
 		Vector3 operator*(float scalar);
 
 		//Methods
-		void printOnConsole() const;									//prints the Vector on the console
+		void printOnConsole() const;   //prints the Vector on the console
 
 	};
 
